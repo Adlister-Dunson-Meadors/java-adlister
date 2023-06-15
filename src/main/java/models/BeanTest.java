e573759866a49efb4e0784a5446a5872f9f57542
Jakira Dunson <jakiradunson@gmail.com>
2023-06-15 14:01:52 -0500

    javabeans excersice quotes

--- src/main/java/models/BeanTest.java
+++ src/main/java/models/BeanTest.java
@@ -1,7 +1,21 @@
 package models;
 
+import java.util.ArrayList;
+
 public class BeanTest {
     public static void main(String[] args) {
-        Album album1 = new Album();
+        Album YHLQMELG = new Album();
+        YHLQMELG.setAlbumName("YHLQMELG");
+        YHLQMELG.setGenre("Latin");
+        YHLQMELG.setArtist("Bad Bunny");
+        YHLQMELG.setReleaseData(2020);
+
+        System.out.println(YHLQMELG);
+
+        ArrayList<Quote> quotes = new ArrayList<>();
+        Author franklin = new Author(1, "Franklin D.", "Roosevelt");
+
+        quote = new Quote(1, "The only thing we have to fear is fear itself.", 1)
+
     }
 }
